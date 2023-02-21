dd4c246ea4665f64472590f99ee9078cdd77c13a
titus-360 <murithititus510@gmail.com>
2023-02-21 03:43:42 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by mbah
-	 * Return: 0
-	 */
-
-
-	int main(void)
-	{
-	_putchar('_');
+/**
+ * main - Entry point
+ * Description: putchar finction
+ * Return: 0;
+ */
+int main(void)
+{	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -17,6 +14,6 @@
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return (0);
-	}
 
+	return (0);
+}
