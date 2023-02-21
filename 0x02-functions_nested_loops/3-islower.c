46c57e241ed95b3272fa0750bb3374244990aa09
titus-360 <murithititus510@gmail.com>
2023-02-21 04:03:41 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_islower - prints 1 or 0 depending on input
+ *@c: first parameter
+ *Description: lower letter only
+ *Return: 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
